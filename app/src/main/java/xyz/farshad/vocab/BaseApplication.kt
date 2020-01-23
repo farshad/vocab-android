132479d0a3a10bde5a0b9e6ef67b9284b32fb481
farshad ahangari <farshad.ahg@gmail.com>
2020-01-23 16:59:20 +0330

    solve dagger dependency issues

--- app/src/main/java/xyz/farshad/vocab/BaseApplication.kt
+++ app/src/main/java/xyz/farshad/vocab/BaseApplication.kt
@@ -2,6 +2,7 @@ package xyz.farshad.vocab
 
 import dagger.android.AndroidInjector
 import dagger.android.support.DaggerApplication
+import xyz.farshad.vocab.di.DaggerAppComponent
 
 class BaseApplication : DaggerApplication() {
     override fun applicationInjector(): AndroidInjector<out DaggerApplication> {
