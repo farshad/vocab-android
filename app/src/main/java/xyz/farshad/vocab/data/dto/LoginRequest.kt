f98f8128983bd2e592b10c7fe77dd273b34d54f2
ahangari <farshad.ahg@gmail.com>
2023-10-31 09:45:41 +0330

    set mode for login

--- app/src/main/java/xyz/farshad/vocab/data/dto/LoginRequest.kt
+++ app/src/main/java/xyz/farshad/vocab/data/dto/LoginRequest.kt
@@ -1,3 +1,7 @@
 package xyz.farshad.vocab.data.dto
 
-data class LoginRequest(private val username: String, private val password: String)
+data class LoginRequest(
+    private val username: String,
+    private val password: String,
+    private val mode: String = "ANDROID",
+)
