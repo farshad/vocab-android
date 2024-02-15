6804d5cd31eaccf07baa66d8d89377d9dfcef4db
ahangari <farshad.ahg@gmail.com>
2024-02-15 11:09:00 +0330

    add pager controller layout

---
+++ app/src/main/java/xyz/farshad/vocab/ui/word/PagerControllerBottomSheet.kt
@@ -0,0 +1,6 @@
+package xyz.farshad.vocab.ui.word
+
+class PagerControllerBottomSheet : BottomSheetDialogFragment() {
+//    private var layout: BlockDetailBottomSheetBinding? = null
+    private val binding get() = layout!!
+}
